07f9a82f27e0b81b7475c833464f7586deede300
Bodzenta_Christopher <Bodzenta_Christopher@edu.mcctc.net>
2020-10-29 09:59:48 -0400

    Add project files.

---
+++ XML/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml; // using xml reader
+using System.Xml.Serialization; // used to create the XML Serialization
+using System.IO; // This is needed for the TextWriter
+
+namespace XMLSample2
+{
+    public class Employee //NOTE YO CANNOT SERIALIZE PRIVATE FIELDS
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double SSNumber { get; set; }
+        public DateTime EntryDate { get; set; } // using DateTime method. Predefined method method in .NET Framework
+    }
+}
---
+++ XML/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml; // using xml reader
+using System.Xml.Serialization; // used to create the XML Serialization
+using System.IO; // This is needed for the TextWriter
+
+namespace XMLSample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.ID = 123;
+            emp.Name = "Ninja Nerd";
+            emp.SSNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"Employee ID: {emp.ID} /nEmployee Name: {emp.Name}/nEmployee SS#: {emp.SSNumber}/nDate Created: {emp.EntryDate}");
+
+            //Creating location to store your XML file
+            string FilePath = "C:/Temp/";
+            string FileName = "Ninja.xml";
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+
+            XmlSerializer ser = new XmlSerializer(typef(Employee));
+
+            ser.Serialize(writer, emp);
+            writer.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
